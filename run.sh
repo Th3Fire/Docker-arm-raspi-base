d55635cdbb8e08d9b17cfcce68b628415f8220f6
Wuttinan <tazakunk@gmail.com>
2016-10-12 10:31:20 +0200

    update env,run.sh,Dockerfile

--- run.sh
+++ run.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
-data=''
-log=''
-count=0
-mkdir -p logs/all
-while true; do
-        for i in 0 1 2 3
-        do
-                count=`sudo timeout 2 cat -u /dev/ttyUSB$i 2>/dev/null | wc -l`
-		#echo "$count \n"
-		if [ $count != "0" ]
-		then
-			datetime=`date +%Y-%m-%d:%H:%M:%S`
-			data=`timeout 2 cat /dev/ttyUSB$i`
-			echo "$datetime : $data" > logs/log_last_ttyUSB$i.log
-			echo "$datetime : $data" >> logs/all/log_ttyUSB$i.log
-			log=`head -n 1 logs/log_last_ttyUSB$i.log`
-
-               		echo "$log\n"
-		else
-			echo "Can't connect ttyUSB$i\n"
-		fi
-        done
-	echo "========================= \n"
+/usr/local/bin/platformio run -t upload
+for i in 0 1 2 3 4 5 6 7
+do
+	screen -d -m /dev/ttyUSB$i 9600
 done
-
+while true;do
+	device=`platformio device list`
+	echo "$device" > device.log
+	sleep 5s
+done
