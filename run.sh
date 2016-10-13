69ef4042f5c02dc2354113e044bedbd706ea6e08
Wuttinan <tazakunk@gmail.com>
2016-10-13 09:39:56 +0200

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 /usr/local/bin/platformio run -t upload
-for i in 0 1 2 3 4 5 6 7
-do
-	screen -S ttyUSB -d -m /dev/ttyUSB$i
-done
+
 while true;do
 	device=`platformio device list`
 	echo "$device" > device.log
